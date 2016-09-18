b2f6bcfe4bfc5f4132ec6a9ace3ad694c85dcc62
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2016-09-18 22:34:54 +0100

    Initialised JFrame window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Aaron on 18/09/2016.
  */
 public class Main {
+
+    public static void main(String[] args){
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame window = new JFrame();
+                window.setVisible(true);
+            }
+        });
+    }
 }
