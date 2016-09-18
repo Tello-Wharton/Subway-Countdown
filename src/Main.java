25d323651eca5f13790f8118973d7021b256e178
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2016-09-18 22:44:23 +0100

    Added width and height properties

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,32 @@ import java.awt.*;
 /**
  * Created by Aaron on 18/09/2016.
  */
-public class Main {
+public class Main extends JFrame{
+
+    private int width;
+    private int height;
 
     public static void main(String[] args){
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                JFrame window = new JFrame();
-                window.setVisible(true);
+                new Main();
             }
         });
+    }
+
+    public Main(){
+        setVisible(true);
+        add(new Board());
+
+        this.width = 1280;
+        this.height = 720;
+
+
+        init();
+    }
+
+    public void init(){
+        setSize(width,height);
     }
 }
